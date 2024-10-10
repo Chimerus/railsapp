507b439ce865fedbb10f8b854c668dfcb110d67c
Jason C <jlc370@gmail.com>
2024-10-09 23:55:07 -0400

    demo navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "projects", to: "projects#index"
   get "projects/:id", to: "projects#show"
-  get "/", to: "home#index"
+  root to: "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
