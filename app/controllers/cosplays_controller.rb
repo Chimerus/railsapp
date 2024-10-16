0fc4c76ca141676f43d7c07fa8222279c4df0fb7
Jason C <jlc370@gmail.com>
2024-10-15 20:56:05 -0400

    update cosplays index and show

--- app/controllers/cosplays_controller.rb
+++ app/controllers/cosplays_controller.rb
@@ -1,7 +1,9 @@
 class CosplaysController < ApplicationController
   def index
+    @cosplays = Cosplay.all
   end
 
   def show
+    @cosplay = Cosplay.find_by(id: id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_11_140500) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_15_025325) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cosplays", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "cover_image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
