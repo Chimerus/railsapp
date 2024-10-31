235a2b38b2a600e7559c85978d279b80da6adfe1
Jason C <jlc370@gmail.com>
2024-10-31 17:48:00 -0400

    add project create to controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,4 +6,17 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find_by(id: id)
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Article.new(project_params)
+    if @project.save
+      redirect_to @project
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
