86ed6d049903484e338a4add8ba260323b8aac9f
Jason Chan <jlc370@gmail.com>
2024-11-03 13:05:58 -0500

    set params for form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,11 +12,17 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    @project = Article.new(project_params)
+    @project = Project.new(project_params)
     if @project.save
       redirect_to @project
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :cover_image_url, :featured)
   end
 end
