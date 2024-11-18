e7ed695e2c5399a280626712841b1a0e5b393d37
Jason Chan <jlc370@gmail.com>
2024-11-17 23:46:17 -0500

    populate inital random stat array

--- app/javascript/character_generator.js
+++ app/javascript/character_generator.js
@@ -1,1 +1,27 @@
-console.log("hello world")
+function randomStat() {
+    let d1 = Math.floor(Math.random() * 6) + 1;
+    let d2 = Math.floor(Math.random() * 6) + 1;
+    let d3 = Math.floor(Math.random() * 6) + 1;
+    return d1+d2+d3;
+}
+
+function statArray() {
+    var array = [];
+    for (let i = 0; i < 6; i++) {
+        array.push(randomStat());
+    }
+    return array;
+}
+
+function setStats(){
+    let stat = statArray();
+    console.log(stat)
+    document.getElementById('strength').textContent = stat[0];
+    document.getElementById('dexterity').textContent = stat[1];
+    document.getElementById('constitution').textContent = stat[2];
+    document.getElementById('wisdom').textContent = stat[3];
+    document.getElementById('intelligence').textContent = stat[4];
+    document.getElementById('charisma').textContent = stat[5];
+}
+
+setStats();
